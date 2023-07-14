a3b51e0c1f7dab74f6d659b95d45cea4bd809f4c
mohit nankani <mohit@pirimidtech.com>
2023-07-14 16:25:01 +0530

    init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5424fcfe0a62c4dc"; }
+/******/ 		__webpack_require__.h = function() { return "863ffb62613af3ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
